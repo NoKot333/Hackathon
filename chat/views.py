41135ab3ea9449ba6a4c6608c53c827df91d2f46
NoKot333 <nicolateslag@gmail.com>
2023-12-09 00:55:44 +0700

    first commit

---
+++ chat/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    if (request.META['SERVER_PORT']=="9322"):
+        return render(
+            request,
+            'server/index.html'
+        )
+    else:
+        return render(
+            request,
+            'client/home/index.html'
+        )
