76b4ef2867c8d018e69c73a49aea61e74a8bf963
detf0r <ekimenkoip@gmail.com>
2023-12-09 17:11:09 +0700

    Update encrypt.py

--- chat/encrypt.py
+++ chat/encrypt.py
@@ -1,8 +1,26 @@
 from Crypto import Random
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
 import base64
 
-def encrypt_message(a_message , publickey):
+def encrypt_message(a_message):
+	publickey = get_public_key()
 	encrypted_msg = publickey.encrypt(a_message, 32)[0]
 	encoded_encrypted_msg = base64.b64encode(encrypted_msg) # base64 encoded strings are database friendly
 	return encoded_encrypted_msg
+
+def decrypt_message(encoded_encrypted_msg):
+	privatekey = get_private_key()
+	decoded_encrypted_msg = base64.b64decode(encoded_encrypted_msg)
+	decoded_decrypted_msg = privatekey.decrypt(decoded_encrypted_msg)
+	return decoded_decrypted_msg
+
+def get_private_key():
+	private_key = "123"
+	return private_key
+
+def get_public_key():
+    public_key = "123"
+    return public_key
+
